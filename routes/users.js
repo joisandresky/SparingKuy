cab733c7468d06743e37fe52d45280a76c0a0ba4
Jois Andresky <jois.andresky77@gmail.com>
2017-05-18 16:17:28 +0700

    API USER dan Authentications

---
+++ routes/users.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/db');
+
+// Model
+const User = require('../models/user');
+
+// Register
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        regional : req.body.regional,
+        contact: req.body.contact,
+        email:  req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg:'Gagal Mendaftarkan User!'});
+        } else {
+            res.json({success: true, msg:'Sukses Mendaftarkan User!'});
+        }
+    })
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        if(err) throw err;
+
+        if(!user){
+            return res.json({success: false, msg:'User tidak Ditemukan!'});
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if(err) throw err;
+
+            if(isMatch){
+                const token = jwt.sign(user, config.secret, {
+                    expiresIn: 604800 // 1 Minggu
+                });
+
+                res.json({
+                    success: true,
+                    token: 'JWT '+token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email,
+                        regional: user.regional
+                    }
+                });
+            } else {
+                return res.json({success: false, msg:'Password yang anda Masukan Salah!'});
+            }
+        })
+    })
+});
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.json({user: req.user});
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const config = require('./config/db');
+
+// Koneksi ke Database
+mongoose.connect(config.database);
+
+// Cek Koneksi ke database
+mongoose.connection.on('connected', () => {
+    console.log('Terkoneksi dengan database ' + config.database);
+});
+
+// Jika Koneksi Error
+mongoose.connection.on('error', (err) => {
+    console.log('Gagal Koneksi Ke database '+err);
+});
+
+const port = 8080;
+const app = express();
+
+// Routing
+const users = require('./routes/users');
+
+// MW untuk CORS
+app.use(cors());
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// MW untuk Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// MW untuk Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res, next) => {
+    res.send('Invalid  End Point');
+})
+
+app.listen(port, () => {
+    console.log('Server running on Port '+port);
+})
